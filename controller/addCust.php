3512923cbd26b49a9cfb5b45d90365ee22f6dc40
Andres Arango <pipe1411@gmail.com>
2017-04-13 01:42:06 -0400

    path fix for adding customers on nav bar

--- controller/addCust.php
+++ controller/addCust.php
@@ -1,9 +1,9 @@
 <?php
 include ("../config/config.php");
-echo "Hello World";
 $json_cust_list = file_get_contents("php://input");
 
 $cust_list = json_decode($json_cust_list,true);
+echo $cust_list;
 
 file_put_contents("../models/dbModel.php",$cust_list);
 redirect("../viewCustomers.html");
