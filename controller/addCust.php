fee64b4b3c2805e264a0ee598fe7e7dea7aba5fc
Andres Arango <pipe1411@gmail.com>
2017-04-13 23:11:13 -0400

    500 error fix?

--- controller/addCust.php
+++ controller/addCust.php
@@ -1,11 +1,13 @@
 <?php
 //include ("../config/config.php");
-$json_cust_list = $_POST['postData'];
+//$json_cust_list = $_POST['postData'];
 
-$cust_list = json_decode($json_cust_list,true);
-echo $cust_list;
+//$cust_list = json_decode($json_cust_list,true);
+//echo $cust_list;
+
+echo "hello world";
 
 //file_put_contents("../models/dbModel.php",$cust_list);
-redirect("../viewCustomers.html");
+//redirect("../viewCustomers.html");
 
 ?>
